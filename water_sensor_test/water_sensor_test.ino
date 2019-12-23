4466e785d2a437ed900eda9711f5a7d046e07eb9
Kevin Thayyil <kevin.thayyil@gmail.com>
2019-12-23 19:43:38 +0530

    water sensor testing and calibration code

--- water_sensor_test/water_sensor_test.ino
+++ water_sensor_test/water_sensor_test.ino
@@ -1,9 +1,18 @@
+#define WaterLevelPin A0 // The sensor works by taking an analogue Voltage reading 
+#define SensorLength 42 // This is the length of the conducting part of the sensor in mm 
+#define SensorMaxima 566 // this is the max analogue reading (out of 1023) 
+// when the sensor is completely dipped in liquid. 
+#define heightOffset 0 // This is the distance of sensor from base of tank in mm 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600); 
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  float waterVoltage = analogRead(WaterLevelPin);
+  Serial.print(waterVoltage);
+  Serial.print(" | Length (mm): ");
+  float waterLevel = heightOffset + (waterVoltage*SensorLength)/SensorMaxima; // Level of water in mm 
+  Serial.println(waterLevel);
+  delay(100);
 }
